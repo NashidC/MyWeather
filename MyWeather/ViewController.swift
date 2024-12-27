969c87397cfddaa6cdaab87c256bb74f4186ff61
NashidC <nash.chowdhury@gmail.com>
2024-12-26 22:44:32 -0500

    first commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Nashid  on 12/26/24.
+//
+
+import UIKit
+
+//Location: CoreLocation
+//table view
+//custom cell: collection view
+//API / request to get the data 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
